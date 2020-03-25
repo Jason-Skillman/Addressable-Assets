600a57ebe96282e29e97a7589144120c8769b430
Jason <jason_skillman@outlook.com>
2020-03-25 10:28:32 -0400

    Loaded subassets

---
+++ Assets/Scripts/SubAsset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class SubAsset : MonoBehaviour {
+
+    public AssetReference assetRef;
+    //public Mesh mesh;
+
+
+    void Start() {
+        assetRef.LoadAssetAsync<GameObject>().Completed += (operation) => {
+            Addressables.LoadAssetAsync<Mesh>("FancyBowl[Sphere001]").Completed += (operation2) => {
+                Debug.Log("Loaded subasset: " + operation2.Result.name);
+            };
+
+        };
+    }
+
+}

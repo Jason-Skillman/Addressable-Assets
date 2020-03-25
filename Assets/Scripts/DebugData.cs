ef6a830e28b7db9a76b60f382e4a3ee3f8e07146
Jason <jason_skillman@outlook.com>
2020-03-25 09:14:23 -0400

    Created a LoadAsset script

--- Assets/Scripts/DebugData.cs
+++ Assets/Scripts/DebugData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+//using UnityEditor.AddressableAssets.Settings;
 
 public class DebugData : MonoBehaviour {
 
@@ -20,6 +21,12 @@ public class DebugData : MonoBehaviour {
         string playerBuildDataPath = Addressables.PlayerBuildDataPath;
         Debug.Log("PlayerBuildDataPath: " + playerBuildDataPath);
 
+    }
+
+    [ContextMenu("Build Player Content")]
+    public void Build() {
+        //Cant use in play mode
+        //AddressableAssetSettings.BuildPlayerContent();
     }
 
 }
---
+++ Assets/Scripts/LoadAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadAsset : MonoBehaviour {
+
+    public AssetReference assetRef;
+
+
+    void Start() {
+        Addressables.LoadAssetAsync<GameObject>(assetRef).Completed += (operation) => {
+            Debug.Log("Loaded asset: " + operation.Result.name);
+        };
+    }
+
+}

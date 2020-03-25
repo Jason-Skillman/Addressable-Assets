7cf2a06d1117634bf29d869f8b93c1d2fa1b9ca1
Jason <jason_skillman@outlook.com>
2020-03-25 09:47:21 -0400

    Tested the ReleaseInstance method

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class SelfDestruct : MonoBehaviour {
+
+	public float lifetime = 3f;
+
+	void Start() {
+		Invoke("Release", lifetime);
+	}
+
+	void Release() {
+		if(!Addressables.ReleaseInstance(gameObject))
+			Destroy(gameObject);
+	}
+}

5bbb45a41c37c793e7529d055a1349783a8d40c6
Jason <jason_skillman@outlook.com>
2020-03-25 12:55:33 -0400

    Added scene additive loading and unloading

---
+++ Assets/Scripts/SceneAddLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.SceneManagement;
+
+public class SceneAddLoader : MonoBehaviour {
+
+    public AssetReference sceneAssetRef;
+
+
+    void Start() {
+        
+    }
+
+    void Update() {
+
+    }
+
+    [ContextMenu("LoadScene")]
+    public void LoadScene() {
+        sceneAssetRef.LoadSceneAsync(LoadSceneMode.Additive).Completed += (operation) => {
+            if(operation.Status != AsyncOperationStatus.Succeeded) {
+                Debug.Log("Failed");
+                return;
+            }
+
+            Debug.Log("Loaded scene: " + operation.Result.Scene.name);
+        };
+    }
+
+    [ContextMenu("UnloadScene")]
+    public void UnloadScene() {
+        sceneAssetRef.UnLoadScene().Completed += (operation) => {
+            if(operation.Status != AsyncOperationStatus.Succeeded) {
+                Debug.Log("Failed");
+                return;
+            }
+
+            Debug.Log("Unloaded scene: " + operation.Result.Scene.name);
+        };
+
+    }
+
+}

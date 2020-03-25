e4407c1dc14bf780d520008d6860b9d946a7044e
Jason <jason_skillman@outlook.com>
2020-03-25 09:37:55 -0400

    Created a InstatiateAsset script

---
+++ Assets/Scripts/InstantiateAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class InstantiateAsset : MonoBehaviour {
+
+    public AssetReference assetRef;
+
+
+    void Start() {
+        Addressables.InstantiateAsync(assetRef, Vector3.zero, Quaternion.identity).Completed += (operation) => {
+            Debug.Log("Instatiated asset: " + operation.Result.name);
+        };
+    }
+
+}

3ffa6d81f07319377a325bdef126cefa5be97987
Jason <jason_skillman@outlook.com>
2020-03-25 08:49:52 -0400

    Created a DataDebug script

---
+++ Assets/Scripts/DebugData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class DebugData : MonoBehaviour {
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+        string buildPath = Addressables.BuildPath;
+        Debug.Log("BuildPath: " + buildPath);
+
+        string runtimePath = Addressables.RuntimePath;
+        Debug.Log("RuntimePath: " + runtimePath);
+
+        string playerBuildDataPath = Addressables.PlayerBuildDataPath;
+        Debug.Log("PlayerBuildDataPath: " + playerBuildDataPath);
+
+    }
+
+}
